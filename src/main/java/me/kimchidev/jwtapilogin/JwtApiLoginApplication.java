00dd165f40b52333000e19b799caa917de8fa8a6
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-04-26 20:27:06 +0900

    create project

---
+++ src/main/java/me/kimchidev/jwtapilogin/JwtApiLoginApplication.java
@@ -0,0 +1,13 @@
+package me.kimchidev.jwtapilogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApiLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApiLoginApplication.class, args);
+    }
+
+}
